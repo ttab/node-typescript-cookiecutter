2d45a05b09fb55ed27658cd986f2a2d253c4576e
Fredrik Appelberg <fredrik.appelberg@tt.se>
2020-09-02 14:11:09 +0200

    TTified

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-yarn add -D $(cat basic_dev_dependencies.txt)
+npm install -D $(cat basic_dev_dependencies.txt)
 rm -f basic_dev_dependencies.txt
-yarn add -D $(cat basic_dependencies.txt)
+npm install $(cat basic_dependencies.txt)
 rm -f basic_dependencies.txt
 git init
 gitmoji --init
---
+++ {{cookiecutter.directory_name}}/Jenkinsfile
@@ -0,0 +1,5 @@
+@Library("common") _
+
+node {
+  dockerize()
+}
