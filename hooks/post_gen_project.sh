b6c8054e135893bdc544129b5651bf793a90f8ea
Fredrik Appelberg <fredrik.appelberg@tt.se>
2020-09-15 15:03:32 +0200

    src -> lib, npm -> yarn, -gitmoji

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-npm install -D $(cat basic_dev_dependencies.txt)
+yarn add -D $(cat basic_dev_dependencies.txt)
 rm -f basic_dev_dependencies.txt
-npm install $(cat basic_dependencies.txt)
+yarn add $(cat basic_dependencies.txt)
 rm -f basic_dependencies.txt
 git init
-gitmoji --init
 git add -A
